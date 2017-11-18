1b3cc08b9aa202b8875984fe7132042f2467d703
Ygenks <kolxozniko@gmail.com>
2017-11-19 00:29:32 +0300

    Params calculation

--- 4/calculate_parameters.py
+++ 4/calculate_parameters.py
@@ -3,12 +3,14 @@
 import sympy as sym
 import re
 
-l, mu = sym.var('l mu')
-
-P0 = sym.Symbol('P0')
-P1 = sym.Symbol('P1')
-P2 = sym.Symbol('P2')
-P3 = sym.Symbol('P3')
+P000 = sym.Symbol('P000')
+P001 = sym.Symbol('P001')
+P010 = sym.Symbol('P010')
+P011 = sym.Symbol('P011')
+P100 = sym.Symbol('P100')
+P101 = sym.Symbol('P101')
+P110 = sym.Symbol('P110')
+P111 = sym.Symbol('P111')
 
 with open('state_diagram.dot', 'r') as f:
     lines = f.read().splitlines()
@@ -20,10 +22,10 @@ equations = {}
 
 for node in sorted(nodes):
 
-    print('node: {}'.format(node))
-
-    left = [s for s in states if s.split('->')[0][0] == node]
-    right = [s for s in states if s.split('->')[1].split('[')[0][1] == node]
+    left = [s for s in states if s.split('->')[0].strip() == node]
+    right = [
+        s for s in states if s.split('->')[1].split('[')[0].strip() == node
+    ]
 
     left_equation = []
     right_equation = []
@@ -36,88 +38,43 @@ for node in sorted(nodes):
     for r in right:
         r_eq = re.findall(r'"([^"]*)"', r)[0]
 
-        input_node = r.split('->')[0].split()[0]
+        input_node = r.split('->')[0].split()[0].strip()
 
         right_equation.append('P{} * {}'.format(input_node, r_eq))
-
-    print(left_equation)
-    print(right_equation)
 
     left_equation = ' + '.join(left_equation)
     left_equation = 'P{} * ({})'.format(node, left_equation)
     right_equation = ' + '.join(right_equation)
 
     equations['P{}'.format(node)] = '{} - {}'.format(right_equation,
-                                                       left_equation)
+                                                     left_equation)
 
-
-for pair in sorted(equations.items(), key=str):
-    print('{}: {}'.format(*pair))
-
-# equations['P0'] = ' + '.join(equations.keys()) + '-1'
+equations['P000'] = ' + '.join(equations.keys()) + '-1'
 
 system = [
     sym.sympify(expr).subs({
-        l: 25,
-        mu: 4,
+        'l': 25.0,
+        'mu': 4.0
     }) for expr in equations.values()
 ]
 
 result = sym.solve(system, list(equations.keys()))
 
+
+l = 25
+mu = 4
+
+Px = sum([v for k, v in result.items() if str(k)[1] == '1'])
+Py = sum([v for k, v in result.items() if str(k)[2] == '1'])
+Pz = sum([v for k, v in result.items() if str(k)[3] == '1'])
+
+Pdeny = result[P111]
+A = l * (1 - Pdeny)
+
 for pair in sorted(result.items(), key=str):
     print('{}: {}'.format(*pair))
 
-
-# for pair in sorted(equations.items(), key=str):
-#     print('{}: {}'.format(*pair))
-
-# nodes = {}
-# for line in lines:
-
-#     if '->' not in line:
-#         continue
-
-# right = 'P{}'.format(line.split('->')[1][1:5])
-# left = line.split('->')[0]
-# equation = re.findall(r'"([^"]*)"', line)[0]
-
-# if not nodes.get(right):
-#     nodes[right] = 'P{} * ({})'.format(left, equation)
-# else:
-#     nodes[right] += ' + P{} * ({})'.format(left, equation)
-
-# nodes.update((k, '{} - {}'.format(v, k)) for k, v in nodes.items())
-
-# nodes['P0'] = ' + '.join(nodes.keys()) + '-1'
-
-# equations = [
-#     sym.sympify(expr).subs({
-#         l: 25,
-#         mu: 4,
-#     }) for expr in nodes.values()
-# ]
-
-# result = sym.solve(equations, list(nodes.keys()))
-
-# p = 0.75
-# pi1 = 0.7
-# pi2 = 0.65
-
-# Pblock = sum([v for k, v in result.items() if str(k)[1] == '1'])
-# Pblockpi1 = sum([v for k, v in result.items() if str(k)[2] == '2'])
-
-# Lqueue = 1 * sum([v for k, v in result.items() if str(k)[3] == '1']) + 2 * sum(
-#     [v for k, v in result.items() if str(k)[3] == '2'])
-
-# Lc = Lqueue + sum([v for k, v in result.items() if str(k)[2] != '0']) + sum(
-#     [v for k, v in result.items() if str(k)[4] == '1'])
-
-# Wc = Lc / ((1 - p) * (1 - Pblock))
-
-# for pair in sorted(result.items(), key=str):
-#     print('{}: {}'.format(*pair))
-# print('Pblocked: {}'.format(Pblock))
-# print('Lqueue: {}'.format(Lqueue))
-# print(Lc)
-# print('Wc: {}'.format(Wc))
+print('Px: {}'.format(Px))
+print('Py: {}'.format(Py))
+print('Pz: {}'.format(Pz))
+print('A: {}'.format(A))
